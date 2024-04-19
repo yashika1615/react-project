48ac7d73bf394b310c1593511de15f7b7bfb8e89
Yashika1615 <katariay22@gmail.com>
2024-04-19 22:13:39 +0530

    ..

---
+++ react practise/app.js
@@ -0,0 +1,7 @@
+const heading= React.createElement(
+    "h1", 
+    {id:"heading",
+    xyz:"abc"},
+"hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
